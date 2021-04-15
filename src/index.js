89f5b840e1286a5b74f62b7a0299e9de6faa07d8
Minjieun <chewchewchew3@gmail.com>
2021-04-15 20:57:05 +0900

    [ADD] globalStyle & themeProvider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/App";
+import GlobalStyle from "./theme/globalStyles";
 
 ReactDOM.render(
   <Router>
-     <App />
+    <GlobalStyle />
+    <App />
   </Router>,
   document.getElementById("root")
 );
