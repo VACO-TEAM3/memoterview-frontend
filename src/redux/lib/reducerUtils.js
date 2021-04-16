265eb271de8b8626b8231b21382a3db02935a1f7
ha-young <hayeong28@naver.com>
2021-04-16 22:10:58 +0900

    [ADD] redux - reducerUtil

---
+++ src/redux/lib/reducerUtils.js
@@ -0,0 +1,113 @@
+export const makeRelatedActionTypes = type => {
+  return [`${type}_SUCCESS`, `${type}_ERROR`];
+};
+
+const getAllIds = byIdList => {
+  return byIdList.map(item => item.id);
+};
+
+const addById = (newByIdList, state) => {
+  return {
+    ...state.byId,
+    ...newByIdList.reduce((obj, item) => {
+      obj[item.id] = item;
+      return obj;
+    }, {}),
+  };
+};
+
+const removeById = (deleteIds, state) => {
+  const deletedByIdList = { ...state.byId };
+
+  for (const deleteId of deleteIds) {
+    delete deletedByIdList[deleteId];
+  }
+
+  return deletedByIdList;
+};
+
+const addIdInAllIds = (newByIdList, state) => {
+  const newAllIds = state.allIds.concat(getAllIds(newByIdList));
+  const removedDupulicateIds = new Set(newAllIds);
+  return Array.from(removedDupulicateIds);
+};
+
+const removeIdInAllIds = (deleteIds, state) => {
+  const newAllIdsSet = new Set(state.allIds);
+  const deleteIdsSet = new Set(deleteIds);
+
+  for (const id of newAllIdsSet) {
+    if (deleteIdsSet.has(id)) {
+      newAllIdsSet.delete(id);
+    }
+  }
+
+  return Array.from(newAllIdsSet);
+};
+
+export const reducerUtils = {
+  initial: (initialData = {}) => ({
+    loading: false,
+    byId: initialData,
+    allIds: getAllIds(initialData),
+    error: null,
+  }),
+  loading: (prevState = null) => ({
+    loading: true,
+    byId: prevState ? prevState.byId : null,
+    allIds: prevState ? prevState.allIds : null,
+    error: null,
+  }),
+  update: (payload, prevState = null) => ({
+    loading: false,
+    byId: prevState ? addById(payload, prevState) : payload,
+    allIds: prevState ? addIdInAllIds(payload, prevState) : getAllIds(payload),
+    error: null,
+  }),
+  delete: (payload, state) => ({
+    loading: false,
+    byId: removeById(payload, state),
+    allIds: removeIdInAllIds(payload, state),
+    error: null,
+  }),
+  error: (error, prevState = null) => ({
+    loading: false,
+    byId: prevState ? prevState.byId : null,
+    allIds: prevState ? prevState.allIds : null,
+    error: error,
+  }),
+};
+
+export const handleAsyncUpdateStateActionsWithNormalize = (type, keepData = false) => {
+  const [SUCCESS, ERROR] = makeRelatedActionTypes(type);
+
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return reducerUtils.loading(keepData ? state : null);
+      case SUCCESS:
+        return reducerUtils.update(action.payload, keepData ? state : null);
+      case ERROR:
+        return reducerUtils.error(action.payload, keepData ? state : null);
+      default:
+        return state;
+    }
+  };
+};
+
+export const handleAsyncRemoveStateActionsWithNormalize = type => {
+  const [SUCCESS, ERROR] = makeRelatedActionTypes(type);
+  
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return reducerUtils.loading(state);
+      case SUCCESS:
+        return reducerUtils.delete(action.payload, state);
+      case ERROR:
+        return reducerUtils.error(action.payload, state);
+      default:
+        return state;
+    }
+  };
+};
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 
+import projects, { projectsSaga } from "./projects";
 import user, { userSaga } from "./user";
 
 const rootReducer = combineReducers({
   user,
+  projects,
 });
 
 export function* rootSaga() {
-  yield all([userSaga()]);
+  yield all([userSaga(), projectsSaga()]);
 }
 
 export default rootReducer;
