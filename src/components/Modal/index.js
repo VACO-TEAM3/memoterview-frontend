19b4476661faa6e7ea636b74804c960dbcf5b3c4
Minjieun <chewchewchew3@gmail.com>
2021-04-16 16:31:09 +0900

    [FIX] styled-component options

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -21,7 +21,7 @@ const ModalWrapper = styled.header`
     overflow-y: auto;
     top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     z-index: 1;
   }
 `;
