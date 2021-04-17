12a7a61ea0e1ed234ea8f4fcdf77a1d0ec42b085
ha-young <hayeong28@naver.com>
2021-04-18 02:58:42 +0900

    [ADD] project evaluation option controll on ProjectAddModalView Component

--- src/components/ProjectAddModalView/index.js
+++ src/components/ProjectAddModalView/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import ModalView from "../ModalView";
 import ProjectEvaluationOptionAddBar from "../ProjectEvaluationOptionAddBar";
+import ProjectOptionItem from "../ProjectOptionItem";
 
 const EditField = styled.div`
   margin: 50px;
@@ -16,12 +17,11 @@ const Label = styled.div`
 
 const Input = styled.input`
   margin: 5px 20px;
+  padding: 3px 10px 0;
   width: 80%;
   height: 30px;
   font-size: 1rem;
 `;
-
-const OptionItem = styled.div``;
 
 const BtnGroup = styled.div``;
 
@@ -38,20 +38,34 @@ export default function ProjectAddModalView() {
     setEvaluationOptions(evaluationOptions.concat(evaluationOption));
   }
 
+  function handleEvaluationOptionDelete(deleteOption) {
+    setEvaluationOptions(
+      evaluationOptions.filter((option) => option !== deleteOption)
+    );
+  }
+
+  //todo. modalview padding, width, height 상수화
+  // 최대 평가옵션, 면접관 상수화
   return (
     <ModalView padding="20px" width="700px" height="600px">
       <EditField>
         <Label>인터뷰 제목</Label>
-        <Input value={title} onChange={handleTitleChange}/>
+        <Input value={title} onChange={handleTitleChange} />
       </EditField>
       <EditField>
         <Label>평가 옵션</Label>
+        {evaluationOptions.length < 4 && (
+          <ProjectEvaluationOptionAddBar
+            onEvaluationOptionAdd={handleEvaluationOptionAdd}
+          />
+        )}
         {evaluationOptions.map((evaluationOption) => (
-          <OptionItem>{evaluationOption}</OptionItem>
+          <ProjectOptionItem
+            key={evaluationOption + evaluationOptions.length}
+            option={evaluationOption}
+            onOptionDelete={handleEvaluationOptionDelete}
+          />
         ))}
-        <ProjectEvaluationOptionAddBar
-          onEvaluationOptionAdd={handleEvaluationOptionAdd}
-        />
       </EditField>
       <EditField>
         <Label>참여 면접관</Label>
