00f15bb8fcfb0ef036cc9dbe01ebc101b3045e20
ha-young <hayeong28@naver.com>
2021-04-17 19:46:12 +0900

    [ADD] logout button on projects page

--- src/components/ProjectsSideNavBar/index.js
+++ src/components/ProjectsSideNavBar/index.js
@@ -5,15 +5,31 @@ import ProjectsSideNavMenu from "../ProjectsSideNavMenu";
 import UserInfo from "../UserInfo";
 
 const ProjectsSideNavBarWrapper = styled.div`
+  position: relative;
   padding: 60px 30px;
   background-color: ${({ theme }) => theme.SideBarBackground};
 `;
 
-export default function ProjectsSideNavBar({ userInfo, onSideMenuChange }) {
+const LogoutBtn = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 60px;
+  width: 100%;
+  text-align: center;
+  font-size: 1.2rem;
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.Aero};
+  }
+`;
+
+export default function ProjectsSideNavBar({ userInfo, onSideMenuChange, onLogoutBtnClick }) {
   return (
     <ProjectsSideNavBarWrapper>
       <UserInfo userInfo={userInfo}/>
       <ProjectsSideNavMenu onMenuChange={onSideMenuChange}/>
+      <LogoutBtn onClick={onLogoutBtnClick}>Logout</LogoutBtn>
     </ProjectsSideNavBarWrapper>
   );
 }
