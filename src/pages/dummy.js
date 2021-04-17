31b8ce6c840032f553bb504616469cf11ad6e436
ha-young <hayeong28@naver.com>
2021-04-17 12:09:47 +0900

    [ADD] mockApi for getProjects... && apply test

--- src/pages/dummy.js
+++ src/pages/dummy.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 
+import { getJoinedProjects, getMyProjects } from "../redux/reducers/projects";
 import { loginUser } from "../redux/reducers/user";
 
 export default function Dummy() {
   const dispatch = useDispatch();
 
-  function handleClick() {
+  function handleLoginClick() {
     dispatch(loginUser({
       email: "hayeong28@naver.com",
       imgURL: "dfdsfasf",
@@ -14,5 +15,19 @@ export default function Dummy() {
     }));
   }
 
-  return <button onClick={handleClick}>hello friend</button>;
+  function handleGetMyPorjectsClick() {
+    dispatch(getMyProjects("userId"));
+  }
+
+  function handleGetJoinedPorjectsClick() {
+    dispatch(getJoinedProjects("userId"));
+  }
+
+  return (
+    <div>
+      <button onClick={handleLoginClick}>login</button>
+      <button onClick={handleGetMyPorjectsClick}>get my projects</button>
+      <button onClick={handleGetJoinedPorjectsClick}>get joined project</button>
+    </div>
+  );
 }
