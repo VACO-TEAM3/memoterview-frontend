38c006bc4ee3da111560a33c925fb35262ddb43a
ha-young <hayeong28@naver.com>
2021-04-17 21:07:33 +0900

    [ADD] useMenus Custom Hook

---
+++ src/hooks/useMenus.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function useMenus({ defaultMenus, onMenuChange }) {
+  const [menuState, setState] = useState(defaultMenus);
+
+  function handleMenuClick(e) {
+    const selectMenu = e.target.dataset.menu;
+
+    if (menuState !== selectMenu) {
+      setState(selectMenu);
+      onMenuChange && onMenuChange(selectMenu);
+    }
+  }
+
+  function checkActive(state, menuState) {
+    return state === menuState;
+  }
+  
+  return { menuState, handleMenuClick, checkActive };
+}
