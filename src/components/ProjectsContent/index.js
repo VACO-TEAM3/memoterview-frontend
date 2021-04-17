082b95461c70d32d5e2f2c85c24a1d2627c8c2a5
ha-young <hayeong28@naver.com>
2021-04-17 20:57:06 +0900

    [ADD] ProjectStatusMenus component

--- src/components/ProjectsContent/index.js
+++ src/components/ProjectsContent/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function ProjectsContent() {
-  return (<div>ProjectsContent</div>);
+import ProjectStatusMenus from "../ProjectStatusMenus";
+
+const ProjectsContentWrapper = styled.div`
+  padding: 20px;
+`;
+
+export default function ProjectsContent({ onStatusMenuChange }) {
+  return (<ProjectsContentWrapper>
+    <ProjectStatusMenus onStatusMenuChange={onStatusMenuChange}/>
+  </ProjectsContentWrapper>);
 }
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+export function checkActive(state, menu) {
+  return state === menu;
+}
