e7581f4a493662136826e3c65924a04236afd832
ha-young <hayeong28@naver.com>
2021-04-18 00:57:53 +0900

    [ADD] ModalView Styled Component

---
+++ src/components/ModalView/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.div`
+  padding: ${props => props.padding};
+  width: ${props => props.width};
+  height: ${props => props.height};
+  background-color: white;
+  border-radius: 5px;
+`;
