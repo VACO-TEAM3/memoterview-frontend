ccd724c695cafbeccb42ef07c4c5bf5920963df9
ha-young <hayeong28@naver.com>
2021-04-17 23:01:49 +0900

    [ADD] ProjectAddButton Component && apply

--- src/components/ProjectsContent/index.js
+++ src/components/ProjectsContent/index.js
@@ -1,6 +1,8 @@
+
 import React from "react";
 import styled from "styled-components";
 
+import ProjectAddButton from "../ProjectAddButton";
 import ProjectsEntry from "../ProjectsEntry";
 import ProjectStatusMenus from "../ProjectStatusMenus";
 
@@ -8,11 +10,12 @@ const ProjectsContentWrapper = styled.div`
   padding: 20px;
 `;
 
-export default function ProjectsContent({ onStatusMenuChange, projects }) {
+export default function ProjectsContent({ onStatusMenuChange, projects, onProjectAddBtnClick }) {
   return (
     <ProjectsContentWrapper>
       <ProjectStatusMenus onStatusMenuChange={onStatusMenuChange} />
       <ProjectsEntry projects={projects}/>
+      <ProjectAddButton onClick={onProjectAddBtnClick}/>
     </ProjectsContentWrapper>
   );
 }
