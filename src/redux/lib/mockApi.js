9e1e99e59837d5d88ac3f5c9bd3cc3b2fc6156e3
ha-young <hayeong28@naver.com>
2021-04-17 12:09:47 +0900

    [ADD] mockApi for getProjects... && apply test

--- src/redux/lib/mockApi.js
+++ src/redux/lib/mockApi.js
@@ -12,3 +12,47 @@ export function loginAPI({ email, imageURL, name }) {
     }, 1000);
   });
 }
+
+export function getMyProjectsAPI(userId) {
+  console.log("getMyProjectsAPI", userId);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          "id": "sfsdfdsfafewf",
+          "title": "Frontend 주니어 채용",
+          "candidateNum": 30,
+          "createAt": "2021-04-10",
+        },
+        {
+          "id": "fdfsdfdsfsdf",
+          "title": "Backend 주니어 채용",
+          "candidateNum": 14,
+          "createAt": "2021-04-10",
+        }
+      ]);
+    }, 1000);
+  });
+};
+
+export function getJoinedProjectsAPI(userId) {
+  console.log("getJoinedProjectsAPI", userId);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        {
+          "id": "123412341234",
+          "title": "Frontend 주니어 채용99",
+          "candidateNum": 999,
+          "createAt": "2021-04-13",
+        },
+        {
+          "id": "5456547657657567",
+          "title": "Backend 주니어 채용99",
+          "candidateNum": 99,
+          "createAt": "2021-04-13",
+        }
+      ]);
+    }, 1000);
+  });
+};
