400598d4c4715cc04b7f670705cc0810128cb880
Minjieun <chewchewchew3@gmail.com>
2021-04-18 13:28:13 +0900

    [ADD] redirect to mainPage

--- src/containers/LoginPageContainer.js
+++ src/containers/LoginPageContainer.js
@@ -1,26 +1,29 @@
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 import LoginPage from "../pages/LoginPage";
 import { loginUser } from "../redux/reducers/user";
 
 export default function LoginPageContainer() {
   const dispatch = useDispatch();
-  const history = useHistory();
+  const { token } = useSelector(({ user }) => ({
+    token: user.token,
+  }));
 
   function onSuccess(data) {
     const { profileObj } = data;
 
     dispatch(loginUser(profileObj));
-    history.push("/");
   }
   
   function onFailure() {
     alert("fail!");
-    history.push("/login");
   }
   
   return (
-    <LoginPage onSuccess={onSuccess} onFailure={onFailure} />
+    <>
+      <LoginPage onSuccess={onSuccess} onFailure={onFailure} />
+      {token && <Redirect to="/projects" />}
+    </>
   );
 }
--- src/store.js
+++ src/store.js
@@ -4,8 +4,13 @@ import { createLogger } from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 
 import reducer, { rootSaga } from "./redux/reducers";
+import history from "./utils/history";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware({
+  context: {
+    history,
+  },
+});
 
 const middleware = [sagaMiddleware];
 
