3220018af96a9e1217bc22fd9b012fd245db6d83
Minjieun <chewchewchew3@gmail.com>
2021-04-19 05:35:48 +0900

    [ADD] render multiple video

---
+++ src/hooks/useUserMedia.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function useUserMedia(mediaOption) {
+  const [localStream, setLocalStream] = useState(null);
+
+  useEffect(() => {
+    async function getLocalStream(constraints) {
+      try {  
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    
+        setLocalStream(stream);
+        navigator.mediaDevices.getUserMedia(constraints);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    console.log(localStream);
+    if (!localStream) {
+      getLocalStream(mediaOption);
+
+      return;
+    }
+
+    return () => {
+      localStream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    };
+  }, [localStream, mediaOption]);
+
+  return { localStream };
+}
