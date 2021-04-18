8253f186badcde7d5b742b3ee878a5690d956dd2
Minjieun <chewchewchew3@gmail.com>
2021-04-18 13:29:28 +0900

    [ADD] auth router

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -4,14 +4,14 @@ export default function useToken() {
   const [token, setToken] = useState(getToken());
 
   function getToken() {
-    const tokenString = localStorage.getItem("accessToken");
+    const tokenString = localStorage.getItem("authorization");
     const accessToken = JSON.parse(tokenString);
 
     return accessToken;
   }
 
   function saveToken(accessToken) {
-    localStorage.setItem("accessToken", JSON.stringify(accessToken));
+    localStorage.setItem("authorization", JSON.stringify(accessToken));
 
     setToken(accessToken);
   }
