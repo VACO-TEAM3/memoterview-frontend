cb25142555da4059019b185c09de4c92be48dbeb
hychoi <hayeong28@naver.com>
2021-04-19 04:19:39 +0900

    [ADD] watson speech to text basic logic

--- src/pages/VoiceToTextTestPage/index.js
+++ src/pages/VoiceToTextTestPage/index.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import recognizeMicrophone from "watson-speech/speech-to-text/recognize-microphone";
+
+import { getSpeechToTextToken } from "../../api";
 
 export default function VoiceToTextTestPage() {
-  function onClickButton() {
-    
-  }
+  const [text, setText] = useState();
   
+  async function onClickButton() {
+    const token = await getSpeechToTextToken();
+
+    const micRecognizer = recognizeMicrophone({
+      token,
+      model: "ko-KR_BroadbandModel", //ko-KR_NarrowbandModel
+      objectMode: true,
+      format: true,
+      timestamps: true,
+    });
+
+    micRecognizer.on("data", (data) => {
+      console.log(data);
+      setText(data.alternatives[0].transcript);
+    });
+    micRecognizer.on("error", (err) => {
+      console.log(err);
+    });
+  }
+
   return (
     <div>
       <button id="button" onClick={onClickButton}>
         Listen To Microphone
       </button>
-      <div>{this.state.text}</div>
+      <div>{text}</div>
     </div>
   );
 }
