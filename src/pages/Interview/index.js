6d629f231358840f30d08a9d717052a35e07f34e
ha-young <hayeong28@naver.com>
2021-04-19 12:38:46 +0900

    [ADD] success multiple peer connetion

--- src/pages/Interview/index.js
+++ src/pages/Interview/index.js
@@ -1,12 +1,6 @@
-import { useParams } from "react-router-dom";
-
 import Video from "../../components/Video";
 
 export default function Interview() {
-  const params = useParams();
-
-  console.log(params);
-  console.log(25);
   return (
     <Video />
   );
