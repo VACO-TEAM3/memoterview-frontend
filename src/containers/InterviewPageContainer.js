8c145b7e41103de92033c17c64c0b3f2dfbfd580
Minjieun <chewchewchew3@gmail.com>
2021-04-20 09:02:39 +0900

    [REFACTOR] destructuring code for refactor

--- src/containers/InterviewPageContainer.js
+++ src/containers/InterviewPageContainer.js
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 import Peer from "simple-peer";
 import { io } from "socket.io-client";
 
-import { Video } from "../components/Video";
+import Video from "../components/Video";
 import { mediaStreaming } from "../utils/media";
 
 export default function InterviewPageContainer() {
@@ -11,20 +12,27 @@ export default function InterviewPageContainer() {
   const userVideo = useRef();
   const peersRef = useRef([]);
   const peerList = [];
-  const roomID = "happy";
   const socket = io.connect("http://localhost:5000");
+  const { id } = useParams();
 
   useEffect(() => {
-    socket.emit("join room", roomID, async () => {
+    socket.emit("join room", id, async () => {
       const stream = await mediaStreaming.Initialize();
       userVideo.current.srcObject = stream;
       setIsStreaming(true);
     });
 
     socket.on("all users", (users) => {
-      console.log(users);
       users.forEach((userID) => {
-        const peer = createPeer(userID, socket.id, mediaStreaming.getStream());
+        const peer = new Peer({
+          initiator: true,
+          trickle: false,
+          stream: mediaStreaming.getStream(),
+        });
+
+        peer.on("signal", (signal) => {
+          socket.emit("sending signal", { userToSignal: userID, callerID: socket.id, signal });
+        });
 
         peersRef.current.push({
           peerID: userID,
@@ -35,21 +43,6 @@ export default function InterviewPageContainer() {
       });
     });
 
-    function createPeer(userToSignal, callerID, stream) {
-      const peer = new Peer({
-        initiator: true,
-        trickle: false,
-        stream,
-      });
-
-      peer.on("signal", (signal) => {
-        console.log(signal);
-        socket.emit("sending signal", { userToSignal, callerID, signal });
-      });
-
-      return peer;
-    }
-
     setPeers(peerList);
   }, []);
 
@@ -59,8 +52,17 @@ export default function InterviewPageContainer() {
     }
 
     socket.on("user joined", (payload) => {
-      console.log(payload);
-      const peer = addPeer(payload.signal, payload.callerID, mediaStreaming.getStream());
+      const peer = new Peer({
+        initiator: false,
+        trickle: false,
+        stream: mediaStreaming.getStream(),
+      });
+
+      peer.on("signal", (signal) => {
+        socket.emit("returning signal", { signal, callerID: payload.callerID });
+      });
+
+      peer.signal(payload.signal);
 
       peersRef.current.push({
         peerID: payload.callerID,
@@ -71,28 +73,10 @@ export default function InterviewPageContainer() {
     });
 
     socket.on("receiving returned signal", (payload) => {
-      console.log(payload);
       const item = peersRef.current.find((p) => p.peerID === payload.id);
 
       item.peer.signal(payload.signal);
     });
-
-    function addPeer(incomingSignal, callerID, stream) {
-      const peer = new Peer({
-        initiator: false,
-        trickle: false,
-        stream,
-      });
-
-      peer.on("signal", (signal) => {
-        console.log(signal);
-        socket.emit("returning signal", { signal, callerID });
-      });
-
-      peer.signal(incomingSignal);
-
-      return peer;
-    }
   }, [isStreaming]);
 
   return (
