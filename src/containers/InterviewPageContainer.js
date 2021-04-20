81b054a2c789abf736e09116001f8a8815c1c02e
Minjieun <chewchewchew3@gmail.com>
2021-04-20 15:26:30 +0900

    [FIX] get page bug of peer

--- src/containers/InterviewPageContainer.js
+++ src/containers/InterviewPageContainer.js
@@ -7,19 +7,25 @@ import Video from "../components/Video";
 import { mediaStreaming } from "../utils/media";
 
 export default function InterviewPageContainer() {
+  const socket = io.connect("http://localhost:5000");
+
   const { id } = useParams();
   const [isStreaming, setIsStreaming] = useState(false);
   const [peers, setPeers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
   const [members, setMembers] = useState([]);
   const userVideo = useRef();
   const peersRef = useRef([]);
   const peerList = [];
-  const socket = io.connect("http://localhost:5000");
 
   useEffect(() => {
     // 리덕스에서 받아온 is opened가 FALSE일 경우
     // socket.emit("createRoom", ({ creatorID, intervieweeID, id }));
-    socket.emit("requestJoin", id, async () => {
+    socket.emit("requestJoin", id, async (error) => {
+      if (error) {
+        setErrorMessage(error);
+      }
+
       const stream = await mediaStreaming.Initialize();
       userVideo.current.srcObject = stream;
 
@@ -39,8 +45,8 @@ export default function InterviewPageContainer() {
     if (!isStreaming) {
       return;
     }
-
-    members.forEach((userID) => {
+    
+    members.forEach((memberID) => {
       const peer = new Peer({
         initiator: true,
         trickle: false,
@@ -48,11 +54,12 @@ export default function InterviewPageContainer() {
       });
 
       peer.on("signal", (signal) => {
-        socket.emit("sendingSignal", { callee: userID, callerID: socket.id, signal });
+        console.log("sendingSignal", socket.id);
+        socket.emit("sendingSignal", { calleeID: memberID, callerID: socket.id, signal });
       });
 
       peersRef.current.push({
-        peerID: userID,
+        peerID: memberID,
         peer,
       });
 
@@ -62,6 +69,7 @@ export default function InterviewPageContainer() {
     setPeers(peerList);
 
     socket.on("sendingForUsers", ({ signal, callerID }) => {
+      console.log("sendingForServer", callerID);
       const peer = new Peer({
         initiator: false,
         trickle: false,
@@ -83,8 +91,9 @@ export default function InterviewPageContainer() {
     });
 
     socket.on("receivingReturnedSignal", ({ signal, calleeID }) => {
+      console.log("receiving", calleeID);
       const item = peersRef.current.find((p) => p.peerID === calleeID);
-
+      console.log(item);
       item.peer.signal(signal);
     });
   }, [isStreaming]);
