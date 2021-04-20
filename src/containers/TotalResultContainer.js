29f0df33fab50b0ccaf340aa826a4fe5325797b1
hyerichung <hyerichungonearth@gmail.com>
2021-04-21 00:07:21 +0900

    [ADD] TotalResult page, container, intervieweeAddModalView

---
+++ src/containers/TotalResultContainer.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import TotalResult from "../pages/TotalResult";
+
+export default function TotalResultContainer() {
+
+  return (
+    <TotalResult/>
+  );
+};
