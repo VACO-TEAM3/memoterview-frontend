9a7ca41ec3799d47fb3e45f69a2759c15d37fe88
hyerichung <hyerichungonearth@gmail.com>
2021-04-20 10:47:03 +0900

    [ADD] projectsContainer, projects, getMyProjectsAPI

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,42 +2,39 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import LoginPageContainer from "../../containers/LoginPageContainer";
+import ProjectsContainer from "../../containers/ProjectsContainer";
 import Layout from "../../Layout";
 import theme from "../../Layout/theme/theme";
 import Dummy from "../../pages/dummy";
 import Interview from "../../pages/Interview";
 import Projects from "../../pages/Projects";
 import VoiceToTextTestPage from "../../pages/VoiceToTextTestPage";
+import AuthRoute from "../AuthRoute";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Dummy />
-          </Route>
-          <Route path="/login">
-            <LoginPageContainer />
-          </Route>
-          <Route path="/projects" exact>
-            <Projects />
-          </Route>
-          <Route path="/projects/:id">
-            <Interview />
-          </Route>
-          <Route path="/search">
-            <Dummy />
-          </Route>
-          <Route path="/result/:id">
-            <Dummy />
-          </Route>
-          <Route path="/voiceTest">
-            <VoiceToTextTestPage />
-          </Route>
-          <Redirect to="/login" />
-        </Switch>
-      </Layout>
+      <Switch>
+        <Route path="/" exact>
+          <LoginPageContainer />
+        </Route>
+        <AuthRoute path="/projects">
+          <ProjectsContainer />
+        </AuthRoute>
+        <AuthRoute path="/projects/:id">
+          <Dummy />
+        </AuthRoute>
+        <AuthRoute path="/result/:id">
+          <Dummy />
+        </AuthRoute>
+        <AuthRoute path="/interview">
+          <Dummy />
+        </AuthRoute>
+        <AuthRoute path="/search">
+          <Dummy />
+        </AuthRoute>
+        <Redirect to="/" />
+      </Switch>
     </ThemeProvider>
   );
 }
