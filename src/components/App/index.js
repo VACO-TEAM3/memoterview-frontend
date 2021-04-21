c789ec56d174805f79d9bc6d7bb25208a2fea51c
Minjieun <chewchewchew3@gmail.com>
2021-04-21 14:02:16 +0900

    After rebasing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,18 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
-import InterviewPageContainer from "../../containers/InterviewPageContainer";
 import LoginPageContainer from "../../containers/LoginPageContainer";
 import ProjectsContainer from "../../containers/ProjectsContainer";
 import theme from "../../Layout/theme/theme";
 import Dummy from "../../pages/dummy";
+import Interview from "../../pages/Interview";
 import Projects from "../../pages/Projects";
 import VoiceToTextTestPage from "../../pages/VoiceToTextTestPage";
 import AuthRoute from "../AuthRoute";
 
-function App() {
+export default function App() {
   return (
     <ThemeProvider theme={theme}>
-<<<<<<< HEAD
       <Switch>
         <Route path="/" exact>
           <LoginPageContainer />
@@ -38,33 +37,6 @@ function App() {
         </Route>
         <Redirect to="/" />
       </Switch>
-=======
-      <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Dummy />
-          </Route>
-          <Route path="/login">
-            <LoginPageContainer />
-          </Route>
-          <Route path="/projects" exact>
-            <Projects />
-          </Route>
-          <Route path="/projects/:id">
-            <InterviewPageContainer />
-          </Route>
-          <Route path="/search">
-            <Dummy />
-          </Route>
-          <Route path="/result/:id">
-            <Dummy />
-          </Route>
-          <Redirect to="/login" />
-        </Switch>
-      </Layout>
->>>>>>> [ADD] connect to camera
     </ThemeProvider>
   );
 }
-
-export default App;
