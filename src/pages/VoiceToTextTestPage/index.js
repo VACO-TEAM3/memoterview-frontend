eb585052603d387f4805164172f5c102c92c04f2
ha-young <hayeong28@naver.com>
2021-04-22 01:05:40 +0900

    [ADD] translate && work flow complete on record btn process

--- src/pages/VoiceToTextTestPage/index.js
+++ src/pages/VoiceToTextTestPage/index.js
@@ -11,14 +11,62 @@ export default function VoiceToTextTestPage() {
 
   const [question, setQuestion] = useState([]);
   const [answer, setAnswer] = useState([]);
-  const [isInterviewee, setIsInterviewee] = useState(false);
-  const [userId, setUserId] = useState(null);
+
+  const [recogText, setRecogText] = useState([]);
+
+  const recogTextRef = useRef(recogText);
   const isRecordingRef = useRef(false);
   const recognitionRef = useRef(null);
+  const isIntervieweeRef = useRef(false);
   const recordBtnElement = useRef();
+  const userIdRef = useRef(null);
+
   const [recordBtnState, setRecordBtnState] = useState(
     RECORD_STATE_TYPE.INTERVIEW_BEFORE
   );
+
+  const startRecognitionRecord = useCallback(() => {
+    if (!SpeechRecognition) {
+      console.error("Speech recognition not supported 😢 (Use Chrome Browser)");
+      return;
+    }
+
+    if (isRecordingRef.current) {
+      return;
+    }
+
+    console.log("recognition start");
+
+    const recognition = new SpeechRecognition();
+
+    recognition.lang = "ko";
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = () => {
+      setRecogText([]);
+    };
+
+    recognition.onresult = (event) => {
+      const transcript = [...event.results].reduce(
+        (acc, result) => acc + result[0].transcript,
+        ""
+      );
+
+      setRecogText(recogText.concat(transcript));
+    };
+
+    recognition.start();
+
+    recognitionRef.current = recognition;
+    isRecordingRef.current = true;
+  }, []);
+
+  const stopRecognitionRecord = useCallback(() => {
+    isRecordingRef.current = false;
+    recognitionRef.current && recognitionRef.current.stop();
+    recognitionRef.current = null;
+  }, []);
 
   const setNextRecordBtnState = useCallback(() => {
     switch (recordBtnState) {
@@ -27,10 +75,12 @@ export default function VoiceToTextTestPage() {
         break;
       case RECORD_STATE_TYPE.QUESTION_BEFORE:
         setRecordBtnState(RECORD_STATE_TYPE.QUESTIONING);
-        socket.emit("question", { userId });
+        startRecognitionRecord();
+        socket.emit("question", { userId: userIdRef.current });
         break;
       case RECORD_STATE_TYPE.QUESTIONING:
         setRecordBtnState(RECORD_STATE_TYPE.ANSWER_BEFORE);
+        stopRecognitionRecord();
         break;
       case RECORD_STATE_TYPE.ANSWER_BEFORE:
         setRecordBtnState(RECORD_STATE_TYPE.ANSWERING);
@@ -41,14 +91,51 @@ export default function VoiceToTextTestPage() {
         socket.emit("endAnswer");
         break;
       case RECORD_STATE_TYPE.SAVING:
-        socket.emit("sendAnswer", { answer: "test" });
         setRecordBtnState(RECORD_STATE_TYPE.QUESTION_BEFORE);
         break;
       default:
         setRecordBtnState(RECORD_STATE_TYPE.QUESTION_BEFORE);
         break;
     }
-  }, [recordBtnState]);
+  }, [recordBtnState, socket, startRecognitionRecord, stopRecognitionRecord, userIdRef]);
+
+  useEffect(() => {
+    console.log("set Socket Event");
+
+    socket.on("preventButton", () => {
+      recordBtnElement.current.disabled = true;
+    });
+
+    socket.on("intervieweeStartAnswer", () => {
+      if (isIntervieweeRef.current) {
+        console.log("intervieweeStartAnswer, start Record");
+        startRecognitionRecord();
+      }
+    });
+
+    socket.on("intervieweeEndAnswer", () => {
+      if (isIntervieweeRef.current) {
+        console.log("intervieweeEndAnswer, stop Record");
+        stopRecognitionRecord();
+        console.log("send Anser", recogTextRef.current);
+        socket.emit("sendAnswer", { answer: recogTextRef.current });
+      }
+    });
+
+    socket.on("questionerReceiveAnswer", ({ questionerId, answer }) => {
+      if (questionerId === userIdRef.current) {
+        console.log("questioner receive Answer", answer);
+      }
+    });
+
+    socket.on("error", ({ message }) => {
+      alert(message);
+    });
+  }, [socket, startRecognitionRecord, stopRecognitionRecord]);
+
+  useEffect(() => {
+    recogTextRef.current = recogText;
+  }, [recogText]);
 
   function handleClickRecord() {
     setNextRecordBtnState();
@@ -71,64 +158,6 @@ export default function VoiceToTextTestPage() {
     };
   }, [handleKeyDown]);
 
-  // Speech Recognition 관련 Side Effect
-  useEffect(() => {
-    if (!SpeechRecognition) {
-      console.error("Speech recognition not supported 😢 (Use Chrome Browser)");
-      return;
-    }
-
-    if (
-      recordBtnState === RECORD_STATE_TYPE.QUESTIONING ||
-      recordBtnState === RECORD_STATE_TYPE.ANSWERING
-    ) {
-      if (isRecordingRef.current) {
-        return;
-      }
-
-      console.log("rerendering");
-
-      const recognition = new SpeechRecognition();
-
-      recognition.lang = "ko";
-      recognition.continuous = true;
-      recognition.interimResults = true;
-
-      recognition.onstart = () => {
-        console.log(
-          `Voice recognition started. Record State ${recordBtnState}`
-        );
-      };
-
-      recognition.onresult = (event) => {
-        const transcript = [...event.results].reduce(
-          (acc, result) => acc + result[0].transcript,
-          ""
-        );
-
-        switch (recordBtnState) {
-          case RECORD_STATE_TYPE.QUESTIONING:
-            setQuestion(question.concat(transcript));
-            break;
-          case RECORD_STATE_TYPE.ANSWERING:
-            setAnswer(answer.concat(transcript));
-            break;
-          default:
-            setQuestion(question.concat(transcript));
-        }
-      };
-
-      recognition.start();
-
-      recognitionRef.current = recognition;
-      isRecordingRef.current = true;
-    } else {
-      isRecordingRef.current = false;
-      recognitionRef.current && recognitionRef.current.stop();
-      recognitionRef.current = null;
-    }
-  }, [answer, question, recordBtnState]);
-
   function handleJoinInterviewer() {
     socket.emit("requestJoinRoom", {
       roomID: "test",
@@ -137,7 +166,7 @@ export default function VoiceToTextTestPage() {
       },
     });
     const uuid = uuidv4();
-    setUserId(uuid);
+    userIdRef.current = uuid;
     console.log("join socket as intervewer", uuid);
     alert("인터뷰어로 참가" + uuid);
   }
@@ -149,38 +178,10 @@ export default function VoiceToTextTestPage() {
         isInterviewee: true,
       },
     });
-    setIsInterviewee(true);
+    isIntervieweeRef.current = true;
     console.log("join socket as interviewee");
     alert("면접자로 참가");
   }
-
-  useEffect(() => {
-    socket.on("preventButton", () => {
-      recordBtnElement.current.disabled = true;
-    });
-
-    socket.on("intervieweeStartAnswer", () => {
-      if (isInterviewee) {
-        console.log("intervieweeStartAnswer, start Record");
-      }
-    });
-
-    socket.on("intervieweeEndAnswer", () => {
-      if (isInterviewee) {
-        console.log("intervieweeEndAnswer, stop Record");
-      }
-    });
-
-    socket.on("questionerReceiveAnswer", ({ questionerId, answer }) => {
-      if (questionerId === userId) {
-        console.log("questioner receive Answer", answer);
-      }
-    });
-
-    socket.on("error", ({ message }) => {
-      alert(message);
-    });
-  });
 
   return (
     <div>
@@ -192,13 +193,12 @@ export default function VoiceToTextTestPage() {
       </button>
       <hr />
       <br />
-      {!isInterviewee && (
+      {!isIntervieweeRef.current && (
         <button id="button" onClick={handleClickRecord} ref={recordBtnElement}>
           {BUTTON_NAME[recordBtnState]}
         </button>
       )}
-      <h1>질문 : {question.join(" ")}</h1>
-      <h2>답변 : {answer.join(" ")}</h2>
+      <h1>녹음 텍스트 : {recogText.join(" ")}</h1>
     </div>
   );
 }
