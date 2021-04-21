fa8d43c413ce66f45aa2b5e58fd46928c3d38b2c
ha-young <hayeong28@naver.com>
2021-04-21 12:10:39 +0900

    [ADD] kakao newton stt api test with relay server

--- src/pages/VoiceToTextTestPage/index.js
+++ src/pages/VoiceToTextTestPage/index.js
@@ -1,12 +1,39 @@
 import React, { useState } from "react";
+import { ReactMic } from "react-mic";
 import recognizeMicrophone from "watson-speech/speech-to-text/recognize-microphone";
 
 import { getSpeechToTextToken } from "../../api";
 
 export default function VoiceToTextTestPage() {
   const [text, setText] = useState([]);
-  
-  async function onClickButton() {
+  const [record, setRecord] = useState(false);
+
+  function onClickKaKaoButton() {
+    setRecord(!record);
+  }
+
+  function onData(recordedBlob) {
+    console.log("chunk of real-time data is: ", recordedBlob);
+  }
+
+  async function onStop(recordedBlob) {
+    console.log("recordedBlob is: ", recordedBlob);
+
+    console.log(JSON.stringify(recordedBlob));
+
+    let form = new FormData();
+    form.append("file", recordedBlob.blob);
+
+    console.log(form);
+
+    const response = await fetch(`${process.env.REACT_APP_SERVER_PORT}/api/speech-to-text/kakao`, {
+      method: "POST",
+      body: form,
+    });
+  }
+
+
+  async function onClickIBMButton() {
     const { accessToken, serviceUrl } = await getSpeechToTextToken();
 
     const micRecognizer = recognizeMicrophone({
@@ -23,6 +50,7 @@ export default function VoiceToTextTestPage() {
       const msg = data.results[0].alternatives[0].transcript;
       setText(text.concat(msg));
     });
+
     micRecognizer.on("error", (err) => {
       console.log(err);
     });
@@ -30,10 +58,31 @@ export default function VoiceToTextTestPage() {
 
   return (
     <div>
-      <button id="button" onClick={onClickButton}>
-        Listen To Microphone
+      <button id="button" onClick={onClickIBMButton}>
+        IBM 실시간 음성인식
       </button>
-      <div>{text.join(" ")}</div>
+      <button id="button" onClick={onClickKaKaoButton}>
+        {record ? "카카오 음성인식 종료" : "카카오 음성인식 시작"}
+      </button>
+      {/* <button id="button" onClick={onClickGoogleButton}>
+        {record ? "구글 음성인식 종료" : "구글 음성인식 시작"}
+      </button> */}
+      <ReactMic
+        record={record} // defaults -> false.  Set to true to begin recording
+        visualSetting="sinewave" // defaults -> "sinewave".  Other option is "frequencyBars"
+        onStop={onStop} // required - called when audio stops recording
+        onData={onData} // optional - called when chunk of audio data is available
+        strokeColor="#000000" // sinewave or frequency bar color
+        backgroundColor="#FF4081" // background color
+        mimeType="audio/wav" // defaults -> "audio/webm".  Set to "audio/wav" for WAV or "audio/mp3" for MP3 audio format (available in React-Mic-Gold)
+        // echoCancellation={true} // defaults -> false
+        // noiseSuppression={true} // defaults -> false
+        channelCount={1} // defaults -> 2 (stereo).  Specify 1 for mono.
+        // bitRate={256000} // defaults -> 128000 (128kbps).  React-Mic-Gold only.
+        sampleRate={16000} // defaults -> 44100 (44.1 kHz).  It accepts values only in range: 22050 to 96000 (available in React-Mic-Gold)
+        // timeSlice={3000} // defaults -> 4000 milliseconds.  The interval at which captured audio is returned to onData callback (available in React-Mic-Gold).
+      />
+      <h1>{text.join(" ")}</h1>
     </div>
   );
 }
