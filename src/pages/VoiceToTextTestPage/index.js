0d889aabdf106b90bd6bb83b69c09b041a7eceda
ha-young <hayeong28@naver.com>
2021-04-22 01:11:15 +0900

    [FIX] gather reference

--- src/pages/VoiceToTextTestPage/index.js
+++ src/pages/VoiceToTextTestPage/index.js
@@ -14,12 +14,15 @@ export default function VoiceToTextTestPage() {
 
   const [recogText, setRecogText] = useState([]);
 
-  const recogTextRef = useRef(recogText);
-  const isRecordingRef = useRef(false);
-  const recognitionRef = useRef(null);
-  const isIntervieweeRef = useRef(false);
+  const recordsGlobalsRef = useRef({
+    recogText,
+    isRecording: false,
+    recognition: null,
+    isInterviewee: false,
+    userId: null,
+  });
+
   const recordBtnElement = useRef();
-  const userIdRef = useRef(null);
 
   const [recordBtnState, setRecordBtnState] = useState(
     RECORD_STATE_TYPE.INTERVIEW_BEFORE
@@ -31,7 +34,7 @@ export default function VoiceToTextTestPage() {
       return;
     }
 
-    if (isRecordingRef.current) {
+    if (recordsGlobalsRef.current.isRecording) {
       return;
     }
 
@@ -58,14 +61,14 @@ export default function VoiceToTextTestPage() {
 
     recognition.start();
 
-    recognitionRef.current = recognition;
-    isRecordingRef.current = true;
+    recordsGlobalsRef.current.recognition = recognition;
+    recordsGlobalsRef.current.isRecording = true;
   }, []);
 
   const stopRecognitionRecord = useCallback(() => {
-    isRecordingRef.current = false;
-    recognitionRef.current && recognitionRef.current.stop();
-    recognitionRef.current = null;
+    recordsGlobalsRef.current.isRecording = false;
+    recordsGlobalsRef.current.recognition && recordsGlobalsRef.current.recognition.stop();
+    recordsGlobalsRef.current.recognition = null;
   }, []);
 
   const setNextRecordBtnState = useCallback(() => {
@@ -76,7 +79,7 @@ export default function VoiceToTextTestPage() {
       case RECORD_STATE_TYPE.QUESTION_BEFORE:
         setRecordBtnState(RECORD_STATE_TYPE.QUESTIONING);
         startRecognitionRecord();
-        socket.emit("question", { userId: userIdRef.current });
+        socket.emit("question", { userId: recordsGlobalsRef.current.userId });
         break;
       case RECORD_STATE_TYPE.QUESTIONING:
         setRecordBtnState(RECORD_STATE_TYPE.ANSWER_BEFORE);
@@ -97,7 +100,7 @@ export default function VoiceToTextTestPage() {
         setRecordBtnState(RECORD_STATE_TYPE.QUESTION_BEFORE);
         break;
     }
-  }, [recordBtnState, socket, startRecognitionRecord, stopRecognitionRecord, userIdRef]);
+  }, [recordBtnState, socket, startRecognitionRecord, stopRecognitionRecord]);
 
   useEffect(() => {
     console.log("set Socket Event");
@@ -107,23 +110,23 @@ export default function VoiceToTextTestPage() {
     });
 
     socket.on("intervieweeStartAnswer", () => {
-      if (isIntervieweeRef.current) {
+      if (recordsGlobalsRef.current.isInterviewee) {
         console.log("intervieweeStartAnswer, start Record");
         startRecognitionRecord();
       }
     });
 
     socket.on("intervieweeEndAnswer", () => {
-      if (isIntervieweeRef.current) {
+      if (recordsGlobalsRef.current.isInterviewee) {
         console.log("intervieweeEndAnswer, stop Record");
         stopRecognitionRecord();
-        console.log("send Anser", recogTextRef.current);
-        socket.emit("sendAnswer", { answer: recogTextRef.current });
+        console.log("send Anser", recordsGlobalsRef.current.recogText);
+        socket.emit("sendAnswer", { answer: recordsGlobalsRef.current.recogText });
       }
     });
 
     socket.on("questionerReceiveAnswer", ({ questionerId, answer }) => {
-      if (questionerId === userIdRef.current) {
+      if (questionerId === recordsGlobalsRef.current.userId) {
         console.log("questioner receive Answer", answer);
       }
     });
@@ -134,7 +137,7 @@ export default function VoiceToTextTestPage() {
   }, [socket, startRecognitionRecord, stopRecognitionRecord]);
 
   useEffect(() => {
-    recogTextRef.current = recogText;
+    recordsGlobalsRef.current.recogText = recogText;
   }, [recogText]);
 
   function handleClickRecord() {
@@ -166,7 +169,7 @@ export default function VoiceToTextTestPage() {
       },
     });
     const uuid = uuidv4();
-    userIdRef.current = uuid;
+    recordsGlobalsRef.current.userId = uuid;
     console.log("join socket as intervewer", uuid);
     alert("인터뷰어로 참가" + uuid);
   }
@@ -178,7 +181,7 @@ export default function VoiceToTextTestPage() {
         isInterviewee: true,
       },
     });
-    isIntervieweeRef.current = true;
+    recordsGlobalsRef.current.isInterviewee = true;
     console.log("join socket as interviewee");
     alert("면접자로 참가");
   }
@@ -193,7 +196,7 @@ export default function VoiceToTextTestPage() {
       </button>
       <hr />
       <br />
-      {!isIntervieweeRef.current && (
+      {!recordsGlobalsRef.current.isInterviewee && (
         <button id="button" onClick={handleClickRecord} ref={recordBtnElement}>
           {BUTTON_NAME[recordBtnState]}
         </button>
