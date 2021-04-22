034c1d2eca845df5022d730221bb0df66b65ce15
hyerichung <hyerichungonearth@gmail.com>
2021-04-23 01:43:06 +0900

    [ADD] Page header setting

--- src/pages/IntervieweeDetail/index.js
+++ src/pages/IntervieweeDetail/index.js
@@ -17,11 +17,24 @@ const MainLayoutWrapper = styled.div`
 
 const Header = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: 80%;
-  height: 15vh;
+  width: 85%;
+  height: 10vh;
   background-color: yellow;
+`;
+
+const UserIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 2vw;
+  background-color: white;
+`;
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1vw;
+  background-color: coral;
 `;
 
 const Navbar = styled.div`
@@ -34,9 +47,37 @@ const Navbar = styled.div`
 
 const Main = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
   height: 85vh;
   background-color: green;
+`;
+
+const UserName = styled.div`
+  font-size: 1.2rem;
+  margin: 0.5vh;
+`;
+
+const UserEmail = styled.div`
+  font-size: 1rem;
+  margin: 0.5vh;
+`;
+
+const FinalScore = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 80%;
+  font-size: 1rem;
+  background-color: skyblue;
+`;
+
+const EvaluationDetailWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 85%;
+  background-color: hotpink;
 `;
 
 export default function IntervieweeDetail() {
@@ -48,14 +89,23 @@ export default function IntervieweeDetail() {
   return (
     <>
       <MainLayoutWrapper>
-
         <Navbar />
-
         <Header>
-          <UserIcon></UserIcon>
+          <UserIconWrapper>
+            <UserIcon />
+          </UserIconWrapper>
+          <UserInfoWrapper>
+            <UserName>유저이름..</UserName>
+            <UserEmail>유저 이메일..</UserEmail>
+          </UserInfoWrapper>
+          <FinalScore>
+            최종 평가 ★★★★★
+          </FinalScore>
         </Header>
 
-        <Main />
+        <Main>
+          <EvaluationDetailWrapper />
+        </Main>
 
       </MainLayoutWrapper>
     </>
