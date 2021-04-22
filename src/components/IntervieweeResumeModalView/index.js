9bd1bb1104ba90fe738614fef0d4114cfdd97fea
hyerichung <hyerichungonearth@gmail.com>
2021-04-22 17:16:12 +0900

    [ADD] IntervieweeResumeModalView style, [Refactor] modal view

--- src/components/IntervieweeResumeModalView/index.js
+++ src/components/IntervieweeResumeModalView/index.js
@@ -5,9 +5,47 @@ import { Document, Page, pdfjs } from "react-pdf";
 import styled from "styled-components";
 
 import ModalView from "../ModalView";
-const BtnGroup = styled.div``;
 
-export default function IntervieweeResumeModalView({ url, onCancleBtnClick }) {
+const BtnGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 20px;
+  margin: 20px;
+`;
+
+const ClosingBtnWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 0;
+`;
+
+const ClosingButton = styled.button`
+  display: flex;
+  justify-content: center;
+  width: 10%;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+  width:20%;
+  height: 25px;
+  cursor: pointer;
+`;
+
+const PageNumberText = styled.p`
+  font-size: 15px;
+`;
+
+export default function IntervieweeResumeModalView({ onCancleBtnClick }) {
+  // pdf를 위해 필요한 값
+  //: interviewee 정보에서(id로 찾아야겠지..) resumePath --> 아래 Document file에 꼽아주면 된다
+
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
@@ -17,43 +55,47 @@ export default function IntervieweeResumeModalView({ url, onCancleBtnClick }) {
     setNumPages(numPages);
   }
 
-  // 숫자 제어하기
+  // 페이지 넘버에 따라 ui 바꿀것인지?
   function handlePrevBtnClick(e) {
     e.preventDefault();
-    if (pageNumber) {
-      setPageNumber(pageNumber - 1);
+
+    if (pageNumber <= numPages) {
+      console.log("first page");
+      return;
     }
+
+    setPageNumber(pageNumber - 1);
   }
 
   function handleNextBtnClick(e) {
     e.preventDefault();
-    if (pageNumber > numPages) {
+
+    if (pageNumber >= numPages) {
+      console.log("finished");
       return;
     }
+
     setPageNumber(pageNumber + 1);
   }
 
-
   return (
-    <ModalView padding="25px" width="700px" height="900px">
-      <Document file="https://memoterview.s3.ap-northeast-2.amazonaws.com/96a66e65e623a6f4c17dba23f69901a0" onLoadSuccess={onDocumentLoadSuccess}>
+    <ModalView padding="5px" width="700px" height="900px">
+      <ClosingBtnWrap>
+        <ClosingButton onClick={onCancleBtnClick}>취소</ClosingButton>
+      </ClosingBtnWrap>
+      <Document
+        file="https://memoterview.s3.ap-northeast-2.amazonaws.com/96a66e65e623a6f4c17dba23f69901a0"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
         <Page pageNumber={pageNumber} />
       </Document>
-      <button
-        onClick={handlePrevBtnClick}
-      >
-        Previous
-      </button>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-      <button
-        onClick={handleNextBtnClick}
-      >
-        Next
-      </button>
-
-      <BtnGroup><button onClick={onCancleBtnClick}>취소</button></BtnGroup>
+      <BtnGroup>
+        <Button onClick={handlePrevBtnClick}>Previous</Button>
+        <PageNumberText>
+          Page {pageNumber} of {numPages}
+        </PageNumberText>
+        <Button onClick={handleNextBtnClick}>Next</Button>
+      </BtnGroup>
     </ModalView>
   );
 };
