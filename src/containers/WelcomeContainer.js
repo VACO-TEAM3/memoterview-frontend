b48a3010326392bedbe39eef8d5e11a41d6cdaed
hyerichung <hyerichungonearth@gmail.com>
2021-04-22 22:35:02 +0900

    [FIX] typo

--- src/containers/WelcomeContainer.js
+++ src/containers/WelcomeContainer.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import styled from "styled-components";
 
-import useInput from "../hooks/useInput";
 import WelcomePage from "../pages/WelcomePage";
 import { setUser } from "../redux/reducers/user";
 
 export default function WelcomeContainer() {
-  const { user: { userData: { username, email } } } = useSelector(({ user }) => ({ user }));
   const dispatch = useDispatch();
 
   const [newUserInfo, setNewUserInfo] = useState({ email: "", name: "" });
