79b63e8056e5e08081e22f0132990a39adbceb72
hyerichung <hyerichungonearth@gmail.com>
2021-04-23 03:05:36 +0900

    [ADD] intervieweeDetailNavBar

---
+++ src/components/IntervieweeDetails/IntervieweeDetailNavBar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Navbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 5vh;
+  background-color: pink;
+`;
+
+function intervieweeDetailNavBar() {
+  return (
+    <Navbar>
+      <button>뒤로가기</button>
+      <button>pdf</button>
+    </Navbar>
+  );
+}
+
+export default intervieweeDetailNavBar;
