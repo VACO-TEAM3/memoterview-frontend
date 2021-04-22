62b79095711aac7e06271d6f953036ec78aed32f
hyerichung <hyerichungonearth@gmail.com>
2021-04-23 04:01:07 +0900

    [FIX] typo

--- src/containers/IntervieweeDetailContainer.js
+++ src/containers/IntervieweeDetailContainer.js
@@ -1,11 +1,6 @@
-import { useDispatch } from "react-redux";
-
 import IntervieweeDetail from "../pages/IntervieweeDetail";
-import { loginUser } from "../redux/reducers/user";
 
 export default function IntervieweeDetailContainer() {
-  const dispatch = useDispatch();
-
   return (
     <>
       <IntervieweeDetail />
