3f501b09baa19173afa09d899f890c7decde120b
hyerichung <hyerichungonearth@gmail.com>
2021-04-23 03:12:53 +0900

    [ADD] intervieweeDetailUserInfo

---
+++ src/components/IntervieweeDetails/IntervieweeDetailUserInfo/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+import UserIcon from "../../UserIcon";
+
+const UserInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1vw;
+  background-color: coral;
+`;
+
+const UserIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 2vw;
+  background-color: white;
+`;
+
+const UserName = styled.div`
+  font-size: 1.2rem;
+  margin: 0.5vh;
+`;
+
+const UserEmail = styled.div`
+  font-size: 1rem;
+  margin: 0.5vh;
+`;
+
+function intervieweeDetailNavBar({ userInfo }) {
+
+  return (
+    <>
+      <UserIconWrapper>
+        <UserIcon />
+      </UserIconWrapper>
+      <UserInfoWrapper>
+        <UserName>{userInfo.userName}</UserName>
+        <UserEmail>{userInfo.userEmail}</UserEmail>
+      </UserInfoWrapper>
+    </>
+  );
+}
+
+export default intervieweeDetailNavBar;
