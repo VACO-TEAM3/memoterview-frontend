8cf0df9faa191736db27857260fc89a78f329093
hyerichung <hyerichungonearth@gmail.com>
2021-04-22 15:46:53 +0900

    [ADD] react-pdf handling

--- src/pages/TotalResult/index.js
+++ src/pages/TotalResult/index.js
@@ -1,39 +1,18 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-import Modal from "../../components/Modal";
-import ProjectAddModalView from "../../components/ProjectAddModalView";
-
 const SplitLayout = styled.div`
   display: grid;
   grid-template-columns: 300px 1fr;
   height: 100vh;
 `;
 
-export default function TotalResult() {
-  const [modalFlag, setModalFlag] = useState(false);
-
-  function handleIntervieweeAddBtnClick() {
-    console.log("click project add button");
-    openAddIntervieweeModal();
-  }
-
-  function closeAddIntervieweeModal() {
-    setModalFlag(false);
-  }
-
-  function openAddIntervieweeModal() {
-    setModalFlag(true);
-  }
+export default function TotalResult({ onIntervieweeAddBtnClick }) {
 
   return (
     <>
-      {modalFlag && (
-        <Modal onClick={closeAddIntervieweeModal}>
-          <ProjectAddModalView />
-        </Modal>
-      )}
-      <div><button onClick={handleIntervieweeAddBtnClick}>CLICK ME!</button></div>
+      <div><button onClick={onIntervieweeAddBtnClick}>UPLOADING MODAL</button></div>
+      <div><button onClick={onIntervieweeAddBtnClick}>GETTING PDF </button></div>
     </>
   );
 }
