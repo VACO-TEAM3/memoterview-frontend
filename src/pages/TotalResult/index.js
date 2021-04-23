55bac8cfc1885883756662464dededcea007ab87
ha-young <hayeong28@naver.com>
2021-04-24 00:04:02 +0900

    [ADD] ToTalResultPage Header, TotalResultContet

--- src/pages/TotalResult/index.js
+++ src/pages/TotalResult/index.js
@@ -4,7 +4,27 @@ import styled from "styled-components";
 import Header from "../../components/Header";
 
 const HeaderNav = styled.div`
+  margin: 0 40px;
   font-size: 1.5rem;
+  cursor: pointer;
+
+  &:first-child {
+    margin-left: 80px;
+  }
+
+  &:last-child {
+    margin-left: auto;
+    margin-right: 80px;
+    text-align: end;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.Aero};
+  }
+`;
+
+const TotalResultContentWrapper = styled.div`
+  margin-top: 4.5rem;
 `;
 
 export default function TotalResult({ onIntervieweeAddBtnClick }) {
@@ -13,9 +33,12 @@ export default function TotalResult({ onIntervieweeAddBtnClick }) {
     <>
       <Header>
         <HeaderNav>Interviewee</HeaderNav>
-        <HeaderNav>Interviewee</HeaderNav>
+        <HeaderNav>Search</HeaderNav>
+        <HeaderNav>Logout</HeaderNav>
       </Header>
-      <div><button onClick={onIntervieweeAddBtnClick}>ADD INTERVIEWEE MODAL</button></div>
+      <TotalResultContentWrapper>
+        <div><button onClick={onIntervieweeAddBtnClick}>ADD INTERVIEWEE MODAL</button></div>
+      </TotalResultContentWrapper>
     </>
   );
 }
