014caaa3a8553aa3da84e703a732771a6d052201
Minjieun <chewchewchew3@gmail.com>
2021-04-23 18:12:56 +0900

    [ADD] video & menu icon to interview page

--- src/components/SubVideo/index.js
+++ src/components/SubVideo/index.js
@@ -1,12 +1,6 @@
 import { useEffect, useRef } from "react";
-import styled from "styled-components";
 
-const StyledVideo = styled.video`
-  display: flex;
-  justify-content: center;
-  width: 85%;
-  margin: auto;
-`;
+import StyledVideo from "../shared/StyledVideo";
 
 export default function SubVideo({ peer }) {
   const ref = useRef();
