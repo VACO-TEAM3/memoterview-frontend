920a54887f109fd7af5b0024567d7e1e7d3c67be
ha-young <hayeong28@naver.com>
2021-04-23 23:55:46 +0900

    [FIX] Header Component display flex

--- src/pages/TotalResult/index.js
+++ src/pages/TotalResult/index.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+import Header from "../../components/Header";
+
+const HeaderNav = styled.div`
+  font-size: 1.5rem;
+`;
+
 export default function TotalResult({ onIntervieweeAddBtnClick }) {
 
   return (
     <>
+      <Header>
+        <HeaderNav>Interviewee</HeaderNav>
+        <HeaderNav>Interviewee</HeaderNav>
+      </Header>
       <div><button onClick={onIntervieweeAddBtnClick}>ADD INTERVIEWEE MODAL</button></div>
     </>
   );
