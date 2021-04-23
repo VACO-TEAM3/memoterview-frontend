b94511b04d636bd8f524dd2c57b4e1dcd0be95b6
Minjieun <chewchewchew3@gmail.com>
2021-04-23 14:07:28 +0900

    [ADD] balanced video layout

---
+++ src/components/shared/StyledVideo.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledVideo = styled.video`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: auto;
+  border-radius: 15px;
+  box-shadow: 0 15px 20px rgba(0, 0, 0, 0.15), 0 12px 10px rgba(0, 0, 0, 0.15);
+`;
+
+export default StyledVideo;
