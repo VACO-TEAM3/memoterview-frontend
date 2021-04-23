0e5c25c02ca298bd02d04f4118a0e3a075fbcbbb
ha-young <hayeong28@naver.com>
2021-04-23 23:40:16 +0900

    [ADD] Loading Component

--- src/redux/reducers/projects.js
+++ src/redux/reducers/projects.js
@@ -1,11 +1,11 @@
 import { takeLatest, takeLeading } from "redux-saga/effects";
 
-import { 
-  addMyProjectAPI, 
-  deleteProjectAPI, 
-  getJoinedProjectsAPI, 
-  getMyProjectsAPI, 
-  updateInterviewRoomState, 
+import {
+  addMyProjectAPI,
+  deleteProjectAPI,
+  getJoinedProjectsAPI,
+  getMyProjectsAPI,
+  updateInterviewRoomState,
 } from "../../api";
 import { PROJECT_TYPES } from "../../constants/projects";
 import { changeDateFormat } from "../../utils/date";
@@ -38,7 +38,8 @@ const DELETE_PROJECT_ERROR = "DELETE_PROJECT_ERROR";
 
 // interview룸 오픈, 클로즈 -> action의 payload로 state 넣어주기
 const UPDATE_INTERVIEW_ROOM = BASE_PATH + "UPDATE_INTERVIEW_ROOM";
-const UPDATE_INTERVIEW_ROOM_SUCCESS = BASE_PATH + "UPDATE_INTERVIEW_ROOM_SUCCESS";
+const UPDATE_INTERVIEW_ROOM_SUCCESS =
+  BASE_PATH + "UPDATE_INTERVIEW_ROOM_SUCCESS";
 const UPDATE_INTERVIEW_ROOM_ERROR = BASE_PATH + "UPDATE_INTERVIEW_ROOM_ERROR";
 
 export const getMyProjects = ({ userId, token }) => ({
@@ -65,17 +66,23 @@ export const deleteProject = ({ projectId, token }) => ({
   meta: projectId,
 });
 
-export const updateInterviewRoom = ({ projectId, token, roomState }) => ({ 
-  type: UPDATE_INTERVIEW_ROOM, 
-  payload: { projectId, token, roomState }, 
+export const updateInterviewRoom = ({ projectId, token, roomState }) => ({
+  type: UPDATE_INTERVIEW_ROOM,
+  payload: { projectId, token, roomState },
   meta: projectId,
 });
 
 const getMyProjectsSaga = createPromiseSaga(GET_MY_PROJECTS, getMyProjectsAPI);
-const getJoinedProjectsSaga = createPromiseSaga(GET_JOINED_PROJECTS, getJoinedProjectsAPI);
+const getJoinedProjectsSaga = createPromiseSaga(
+  GET_JOINED_PROJECTS,
+  getJoinedProjectsAPI
+);
 const addMyProjectSaga = createPromiseSaga(ADD_MY_PROJECT, addMyProjectAPI);
 const deleteProjectSaga = createPromiseSaga(DELETE_PROJECT, deleteProjectAPI);
-const updateInterviewRoomSaga = createPromiseSaga(UPDATE_INTERVIEW_ROOM, updateInterviewRoomState);
+const updateInterviewRoomSaga = createPromiseSaga(
+  UPDATE_INTERVIEW_ROOM,
+  updateInterviewRoomState
+);
 
 export function* projectsSaga() {
   yield takeLeading(GET_MY_PROJECTS, getMyProjectsSaga);
@@ -112,41 +119,68 @@ export default function projects(state = initialState, action) {
     case GET_MY_PROJECTS:
     case GET_MY_PROJECTS_SUCCESS:
     case GET_MY_PROJECTS_ERROR: {
-      const newState = getProjectsByProjectType(state, action, PROJECT_TYPES.MY_PROJECTS);
+      const newState = getProjectsByProjectType(
+        state,
+        action,
+        PROJECT_TYPES.MY_PROJECTS
+      );
 
-      return handleAsyncUpdateStateActionsWithNormalize(GET_MY_PROJECTS, true)(newState, action);
+      return handleAsyncUpdateStateActionsWithNormalize(GET_MY_PROJECTS, true)(
+        newState,
+        action
+      );
     }
     case GET_JOINED_PROJECTS:
     case GET_JOINED_PROJECTS_SUCCESS:
     case GET_JOINED_PROJECTS_ERROR: {
-      const newState = getProjectsByProjectType(state, action, PROJECT_TYPES.JOINED_PROJECTS);
+      const newState = getProjectsByProjectType(
+        state,
+        action,
+        PROJECT_TYPES.JOINED_PROJECTS
+      );
 
-      return handleAsyncUpdateStateActionsWithNormalize(GET_JOINED_PROJECTS, true)(newState, action);
+      return handleAsyncUpdateStateActionsWithNormalize(
+        GET_JOINED_PROJECTS,
+        true
+      )(newState, action);
     }
     case ADD_MY_PROJECT:
     case ADD_MY_PROJECT_SUCCESS:
     case ADD_MY_PROJECT_ERROR: {
-      const newState = addProjectsByProjectId(state, action, PROJECT_TYPES.MY_PROJECTS);
-      return handleAsyncUpdateStateActionsWithNormalize(ADD_MY_PROJECT, true)(newState, action);
+      const newState = addProjectsByProjectId(
+        state,
+        action,
+        PROJECT_TYPES.MY_PROJECTS
+      );
+      return handleAsyncUpdateStateActionsWithNormalize(ADD_MY_PROJECT, true)(
+        newState,
+        action
+      );
     }
     case DELETE_PROJECT:
     case DELETE_PROJECT_SUCCESS:
     case DELETE_PROJECT_ERROR: {
       const newState = deleteProjectByProjectId(state, action);
-      
-      return handleAsyncRemoveStateActionsWithNormalize(DELETE_PROJECT, true)(newState, action);
+
+      return handleAsyncRemoveStateActionsWithNormalize(DELETE_PROJECT, true)(
+        newState,
+        action
+      );
     }
     case UPDATE_INTERVIEW_ROOM:
     case UPDATE_INTERVIEW_ROOM_SUCCESS:
     case UPDATE_INTERVIEW_ROOM_ERROR:
-      return handleAsyncUpdateStateActionsWithNormalize(UPDATE_INTERVIEW_ROOM, true)(state, action);
+      return handleAsyncUpdateStateActionsWithNormalize(
+        UPDATE_INTERVIEW_ROOM,
+        true
+      )(state, action);
     default:
       return state;
   }
 }
 
 export function projectIdsToByIdObjs(ids, projectByIds) {
-  return ids.map(projectId => {
+  return ids.map((projectId) => {
     const { id, title, candidateNum, createAt } = projectByIds[projectId];
     const formattedDate = changeDateFormat(createAt, "yyyy-MM-dd");
 
