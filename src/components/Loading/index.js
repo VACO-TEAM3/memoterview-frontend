573f705fed1ee5b897ce3ac8e5b73a8df06f1a52
ha-young <hayeong28@naver.com>
2021-04-23 23:40:16 +0900

    [ADD] Loading Component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoadingWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+
+  background-color: rgba(255, 255, 255, 0);
+`;
+
+function Loading() {
+  return (
+    <LoadingWrapper>
+      <video
+        src="https://cdnl.iconscout.com/lottie/premium/preview-watermark/employee-review-3575379-3004512.mp4"
+        autoplay="autoplay"
+        muted="muted"
+        loop="loop"
+        playsinline=""
+        type="video/mp4"
+        width="350px"
+        height="350px"
+      ></video>
+    </LoadingWrapper>
+  );
+}
+
+export default Loading;
