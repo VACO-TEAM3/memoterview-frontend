eca2868320276fa8b2f9cb614e5e0e6b600c6ecd
Minjieun <chewchewchew3@gmail.com>
2021-04-23 18:12:56 +0900

    [ADD] video & menu icon to interview page

--- src/components/MainVideo/index.js
+++ src/components/MainVideo/index.js
@@ -1,18 +1,9 @@
 import styled from "styled-components";
 
-const MainWrapper = styled.div`
-  video {
-    display: flex;
-    justify-content: center;
-    width: 85%;
-    margin: auto;
-  }
-`;
+import StyledVideo from "../shared/StyledVideo";
 
 export default function MainVideo({ videoRef }) {
   return (
-    <MainWrapper>
-      <video ref={videoRef} autoPlay playsInline />
-    </MainWrapper>
+    <StyledVideo ref={videoRef} autoPlay playsInline />
   );
 }
--- src/components/shared/StyledVideo.js
+++ src/components/shared/StyledVideo.js
@@ -6,7 +6,7 @@ const StyledVideo = styled.video`
   width: 100%;
   margin: auto;
   border-radius: 15px;
-  box-shadow: 0 15px 20px rgba(0, 0, 0, 0.15), 0 12px 10px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 15px 20px rgba(0, 0, 0, 0.2), 0 12px 12px rgba(0, 0, 0, 0.15);
 `;
 
 export default StyledVideo;
