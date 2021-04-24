9adbc2640eec91b4765c70ccd3c7765ab0d7cfd1
ha-young <hayeong28@naver.com>
2021-04-24 14:31:19 +0900

    [ADD] TotalResultIntervieweeRow, Column Component only styled

---
+++ src/components/TotalResultIntervieweeList/TotalResultIntervieweeRow.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: grid;
+  grid-template-columns: 0.25fr 0.3fr 0.4fr 0.05fr;
+  height: 40px;
+  width: 100%;
+  background-color: red;
+`;
