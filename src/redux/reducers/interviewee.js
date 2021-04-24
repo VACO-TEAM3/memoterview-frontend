dcc6f37a2cfdc085af4de732f7bb6346c9f1c11c
ha-young <hayeong28@naver.com>
2021-04-25 03:01:59 +0900

    [ADD] redux - interviewee delete

--- src/redux/reducers/interviewee.js
+++ src/redux/reducers/interviewee.js
@@ -1,7 +1,7 @@
 import { takeLatest, takeLeading } from "@redux-saga/core/effects";
 
-import { createIntervieweeAPI, getIntervieweesApi, updateInterviewee } from "../../api";
-import { handleAsyncUpdateStateActionsWithNormalize } from "../lib/reducerUtils";
+import { createIntervieweeAPI, deleteIntervieweeAPI, getIntervieweesApi, updateInterviewee } from "../../api";
+import { handleAsyncRemoveStateActionsWithNormalize, handleAsyncUpdateStateActionsWithNormalize } from "../lib/reducerUtils";
 import { createPromiseSaga, createPromiseSagaById } from "../lib/sagaUtils";
 
 const BASE_PATH = "INTERVIWEE/";
@@ -20,6 +20,12 @@ export const ADD_NEW_INTERVIEWEE_ERROR = BASE_PATH + "ADD_NEW_INTERVIEWEE_ERROR"
 export const FINISH_INTERVIEW = BASE_PATH + "FINISH_INTERVIEW";
 export const FINISH_INTERVIEW_SUCCESS = BASE_PATH + "FINISH_INTERVIEW_SUCCESS";
 export const FINISH_INTERVIEW_ERROR = BASE_PATH + "FINISH_INTERVIEW_ERROR";
+
+// interviewee 삭제 ->
+export const DELETE_INTERVIEWEE = BASE_PATH + "DELETE_INTERVIEWEES";
+export const DELETE_INTERVIEWEE_SUCCESS = BASE_PATH + "DELETE_INTERVIEWEES_SUCCESS";
+export const DELETE_INTERVIEWEE_ERROR = BASE_PATH + "DELETE_INTERVIEWEES_ERROR";
+
 
 export const getInterviewees = ({ projectId, token }) => ({
   type: GET_INTERVIEWEES,
@@ -44,14 +50,22 @@ export const finishInterview = ({
   meta: interviewee,
 });
 
+export const deleteInterviewee = ({ token, projectId, intervieweeId }) => ({
+  type: DELETE_INTERVIEWEE,
+  payload: { token, projectId, intervieweeId },
+  meta: intervieweeId,
+});
+
 export const getIntervieweesSaga = createPromiseSaga(GET_INTERVIEWEES, getIntervieweesApi);
 export const addNewIntervieweeSaga = createPromiseSaga(ADD_NEW_INTERVIEWEE, createIntervieweeAPI);
 export const finishInterviewSaga = createPromiseSaga(FINISH_INTERVIEW, updateInterviewee);
+export const deleteIntervieweeSaga = createPromiseSaga(DELETE_INTERVIEWEE, deleteIntervieweeAPI);
 
 export function* intervieweeSaga() {
   yield takeLeading(GET_INTERVIEWEES, getIntervieweesSaga);
   yield takeLatest(ADD_NEW_INTERVIEWEE, addNewIntervieweeSaga);
   yield takeLatest(FINISH_INTERVIEW, finishInterviewSaga);
+  yield takeLatest(DELETE_INTERVIEWEE, deleteIntervieweeSaga);
 }
 
 const commentInitialState = {
@@ -95,12 +109,15 @@ export default function interviewees(state = initialState, action) {
     case GET_INTERVIEWEES:
     case GET_INTERVIEWEES_SUCCESS:
     case GET_INTERVIEWEES_ERROR:
-      console.log(action.payload);
       return handleAsyncUpdateStateActionsWithNormalize(GET_INTERVIEWEES, true)(state, action);
     case FINISH_INTERVIEW:
     case FINISH_INTERVIEW_SUCCESS:
     case FINISH_INTERVIEW_ERROR:
       return handleAsyncUpdateStateActionsWithNormalize(FINISH_INTERVIEW, true)(state, action);
+    case DELETE_INTERVIEWEE:
+    case DELETE_INTERVIEWEE_SUCCESS:
+    case DELETE_INTERVIEWEE_ERROR:
+      return handleAsyncRemoveStateActionsWithNormalize(DELETE_INTERVIEWEE, true)(state, action);
     default:
       return state;
   }
