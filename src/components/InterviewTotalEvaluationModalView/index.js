08bf5234ee52b579d24836eefaa7e61aa54b216a
Minjieun <chewchewchew3@gmail.com>
2021-04-24 18:59:35 +0900

    [ADD] rating stars component

--- src/components/InterviewTotalEvaluationModalView/index.js
+++ src/components/InterviewTotalEvaluationModalView/index.js
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+
 import ModalView from "../ModalView";
+import RatingStars from "../RatingStars";
+
+const ViewWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default function InterviewTotalEvaluationModalView() {
   return (
     <ModalView padding="20px" width="500px" height="400px">
-      <div>sayhello</div>
+      <ViewWrapper>
+        <input type="text" />
+        <RatingStars />
+      </ViewWrapper>
     </ModalView>
   );
 }
