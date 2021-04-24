7bb354bee312a44ea49afd2d907ee0afcde72f10
ha-young <hayeong28@naver.com>
2021-04-24 14:30:55 +0900

    [ADD] TotalResultIntervieweeList Component

--- src/components/TotalResultContentTop/index.js
+++ src/components/TotalResultContentTop/index.js
@@ -2,19 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 import Calendar from "../Calendar";
+import TotalResultIntervieweeList from "../TotalResultIntervieweeList";
 
 const TotalResultContentTopWrapper = styled.div`
   display: grid;
   grid-template-columns: 0.4fr 0.6fr;
   grid-gap: 50px;
   height: 35vh;
-  background-color: gray;
-  box-sizing: border-box;
-`;
-
-const TotalResultIntervieweeListWrapper = styled.div`
-  height: 100%;
-  width: 100%;
   box-sizing: border-box;
 `;
 
@@ -22,12 +16,7 @@ export default function TotalResultContentTop({ onIntervieweeAddBtnClick }) {
   return (
     <TotalResultContentTopWrapper>
       <Calendar />
-      <TotalResultIntervieweeListWrapper style={{ backgroundColor: "blue" }}>
-        <div></div>
-        <button onClick={onIntervieweeAddBtnClick}>
-          ADD INTERVIEWEE MODAL
-        </button>
-      </TotalResultIntervieweeListWrapper>
+      <TotalResultIntervieweeList onAddBtnClick={onIntervieweeAddBtnClick}/>
     </TotalResultContentTopWrapper>
   );
 }
---
+++ src/components/TotalResultIntervieweeList/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+import TotalResultIntervieweeListHeader from "./TotalResultIntervieweeListHeader";
+
+const TotalResultIntervieweeListWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  border: 1px solid gray;
+  border-radius: 2px;
+  box-sizing: border-box;
+`;
+
+export default function TotalResultIntervieweeList({ onAddBtnClick }) {
+  return (
+    <TotalResultIntervieweeListWrapper style={{ backgroundColor: "blue" }}>
+      <TotalResultIntervieweeListHeader onAddBtnClick={onAddBtnClick} />
+    </TotalResultIntervieweeListWrapper>
+  );
+}
