c40ef02abfe8719f46f14a1671a2ff126e4b7380
ha-young <hayeong28@naver.com>
2021-04-25 17:49:41 +0900

    [ADD] TotalResultFilterModal View

---
+++ src/components/TotalResultFilterModalView/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ModalView from "../ModalView";
+
+export default function TotalResultFilterModalView() {
+  console.log("hahaha");
+  return (
+    <ModalView padding="20px" width="700px" height="650px">
+      <h1>결과옵션필터</h1>
+    </ModalView>
+  );
+}
