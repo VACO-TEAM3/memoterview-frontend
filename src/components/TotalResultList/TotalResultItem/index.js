9662eb0cb2fe7f429f5754e9a6c85beb3e36d8f2
ha-young <hayeong28@naver.com>
2021-04-25 17:06:26 +0900

    [ADD] TotalResultItem Component

---
+++ src/components/TotalResultList/TotalResultItem/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import TotalResultListColumn from "../TotalResultListColumn";
+import TotalResultListRow from "../TotalResultListRow";
+
+function getAverages(scroes) {
+  return scroes
+    ? scroes.reduce((acc, score) => typeof score === "object"  ? acc + score.score : acc + score, 0) / scroes.length
+    : 0;
+}
+
+function mappedFilterValue({ interviewee, columnItem }) {
+  switch (columnItem) {
+    case "평가":
+      return getAverages(interviewee.comments);
+    case "지원자":
+      return interviewee.name;
+    case "응답점수":
+      return getAverages(interviewee.questions);
+    case "질문개수":
+      return interviewee.questions.length;
+    case "면접시간":
+      return interviewee.interviewDuration;
+    case "면접일자":
+      return interviewee.interviewDate;
+    default:
+      return interviewee.filterScores ? getAverages(interviewee.filterScores[columnItem]) : 0;
+  }
+}
+
+export default function TotalResultItem({ interviewee, columnList }) {
+  console.log("haha", interviewee, columnList);
+  console.log(interviewee.id);
+  return (
+    <TotalResultListRow>
+      {columnList.map((columnItem) => (
+        <TotalResultListColumn key={columnItem}>
+          {mappedFilterValue({ interviewee, columnItem })}
+        </TotalResultListColumn>
+      ))}
+    </TotalResultListRow>
+  );
+}
