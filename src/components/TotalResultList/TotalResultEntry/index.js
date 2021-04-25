bc6f0e261f405025f1b6fba2806a57fefb205761
ha-young <hayeong28@naver.com>
2021-04-25 17:05:53 +0900

    [ADD] TotalResultEntry Component

---
+++ src/components/TotalResultList/TotalResultEntry/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+import TotalResultItem from "../TotalResultItem";
+
+const TotalResultEntryWrapper = styled.div`
+  width: 100%;
+  height: 450px;
+  border: 1px solid gray;
+  overflow-y: scroll;
+`;
+
+export default function TotalResultEntry({
+  interviewees,
+  columnList,
+}) {
+  return (
+    <TotalResultEntryWrapper>
+      {interviewees.map((interviewee) => (
+        <TotalResultItem
+          key={interviewee.id}
+          columnList={columnList}
+          interviewee={interviewee}
+        />
+      ))}
+    </TotalResultEntryWrapper>
+  );
+}
