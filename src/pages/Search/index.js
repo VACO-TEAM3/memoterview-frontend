cc3be9bf891ed9b5435dced352ce43369b008c3f
hyerichung <hyerichungonearth@gmail.com>
2021-04-25 14:48:57 +0900

    [ADD] SearchContainer, Search page

---
+++ src/pages/Search/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export default function Search() {
+
+  return (
+    <>
+      <div>asdfadf</div>
+    </>
+  );
+}
