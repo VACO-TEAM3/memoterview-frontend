9f91d7862a0ff30489fbe1f391a31d5e87760cac
hyerichung <hyerichungonearth@gmail.com>
2021-04-26 17:41:52 +0900

    [ADD] ellipsized interviewee answer

--- src/components/SearchQuestions/interviewee/index.js
+++ src/components/SearchQuestions/interviewee/index.js
@@ -3,23 +3,76 @@ import styled from "styled-components";
 
 const IntervieweeWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  padding: 1vh 2vw;
+  background-color: orange;
+`;
+
+const ScoreWrapper = styled.div`
+  display: flex;
+  background-color: red;
+`;
+
+const InterviweeNameWrapper = styled.div`
+  display: flex;
+
+  background-color: yellow;
+`;
+
+const InterviewDateWrapper = styled.div`
+  display: flex;
+  background-color: green;
+`;
+
+const Label = styled.div`
+  display: flex;
+  margin-right: 0.5vw;
+  background-color: blue;
+`;
+
+
+const DataWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  background-color: green;
+  min-width: 0;
+`;
+
+
+const ResultWrapper = styled.div`
+  display: flex;
+  width: 90%;
+  padding: 0.5vh 0 0.5vw 0;
+  font-size: 18px;
+  border: 1px solid white;
   background-color: pink;
 `;
 
-const TitleWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: green;
-  border: 1px solid white;
+const Span = styled.span`
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  background-color: red;
 `;
 
-export default function Interviewer({ searchResult }) {
+// const ResultBox = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 80%;
+//   background-color: hotpink;
+// `;
+
+export default function Interviewee({ searchResult }) {
+  const ellipsizedAnswewr = searchResult.answer.length > 120
+    ? searchResult.answer.substring(0, 120) + "..."
+    : searchResult.answer;
+
   return (
     <IntervieweeWrapper>
-      <div>점수: {searchResult.score}</div>
-      <div>답변자: {searchResult.intervieweeName}</div>
-      <div>답변날짜: {searchResult.interviewDate}</div>
-      <div>답변: {searchResult.answer}</div>
+      <ResultWrapper><Label>답변자:</Label><DataWrapper>{searchResult.intervieweeName}</DataWrapper></ResultWrapper>
+      <ResultWrapper><Label>답변날짜:</Label><DataWrapper>{searchResult.interviewDate}</DataWrapper></ResultWrapper>
+      <ResultWrapper><Label>답변:</Label><DataWrapper>{ellipsizedAnswewr}</DataWrapper></ResultWrapper>
+      <ResultWrapper><Label>점수:</Label> <DataWrapper>{searchResult.score}</DataWrapper></ResultWrapper>
     </IntervieweeWrapper>
   );
 }
