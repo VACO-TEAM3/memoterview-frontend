d5c8c9f9be238b1b7773e00fb963510b4f62060b
ha-young <hayeong28@naver.com>
2021-04-27 07:22:53 +0900

    [ADD] handleGeneratePdfBtnClick - window.print()

--- src/containers/IntervieweeDetailContainer.js
+++ src/containers/IntervieweeDetailContainer.js
@@ -1,20 +1,21 @@
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
 
-import { STARS } from "../constants/interviewee";
 import IntervieweeDetail from "../pages/IntervieweeDetail";
-import { createStars } from "../utils/createStars";
+
+const Wrapper = styled.div`
+  width: 100vw;
+`;
 
 export default function IntervieweeDetailContainer() {
   const history = useHistory();
 
   const { projectId, intervieweeId } = useParams();
-  const {
-    intervieweeInfo,
-  } = useSelector(({ interviewees: { byId } }) => ({ intervieweeInfo: getIntervieweeInfo(byId[intervieweeId]) }));
+  const { intervieweeInfo } = useSelector(({ interviewees: { byId } }) => ({
+    intervieweeInfo: getIntervieweeInfo(byId[intervieweeId]),
+  }));
 
   function handleGoBackButtonClick(e) {
     e.preventDefault();
@@ -33,16 +34,16 @@ export default function IntervieweeDetailContainer() {
   }
 
   function handleGeneratePdfBtnClick(e) {
-
+    window.print();
   }
 
   return (
-    <div>
+    <Wrapper>
       <IntervieweeDetail
         onGeneratePdfBtnClick={handleGeneratePdfBtnClick}
         onGoBackButtonClick={handleGoBackButtonClick}
         intervieweeInfo={intervieweeInfo}
       />
-    </div>
+    </Wrapper>
   );
 }
