3d8be3a975fc086bdff344401aca89108160bbfa
Minjieun <chewchewchew3@gmail.com>
2021-04-28 15:48:10 +0900

    [FIX] interview page tap shadow

--- src/components/InterviewRightTab/index.js
+++ src/components/InterviewRightTab/index.js
@@ -13,15 +13,16 @@ const TabWrapper = styled.div`
     min-height: 1rem;
     padding-bottom: 2rem;
     background: white;
-    border-radius: 0 0 3px 0;
-    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.22);
+    border-radius: 0 0 0 3px;
+    box-shadow: 10px 19px 15px rgba(0, 0, 0, 0.30), 20px 15px 12px rgba(0, 0, 0, 0.22);
+    z-index: 3;
   }
 `;
 
 export default function InterviewRightTab({ tabName, tabIcon, onClick, children, isOpened }) {
   return (
     <TabWrapper>
-      <TabButton borderRadius="5px 0 0 5px" name={tabName} icon={tabIcon} onClick={onClick} />
+      <TabButton isLeft={false} borderRadius="5px 0 0 5px" name={tabName} icon={tabIcon} onClick={onClick} />
       {isOpened && <div className="tab-content">{children}</div>}
     </TabWrapper>
   );
