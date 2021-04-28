733eb9fc141a7546d7e2e517ebbb6e664c0952d6
Minjieun <chewchewchew3@gmail.com>
2021-04-28 22:24:25 +0900

    [ADD] audio on off icon to videos

---
+++ src/components/VideoContentTag/index.js
@@ -0,0 +1,31 @@
+import { faMicrophoneAlt, faMicrophoneAltSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const VideoContentTagWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 3rem;
+  padding: 0.1rem 0.3rem;
+  background: #1E1E1E90;
+  color: rgba(255, 255, 255);
+  position: absolute;
+  bottom: 0%;
+  left: 0;
+  font-weight: bold;
+  border-radius: 3px;
+  border: 2px solid ${({ color }) => color};
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  z-index: 2;
+  text-align: center;
+`;
+
+export default function VideoContentTag({ peer, children }) {
+  return (
+    <VideoContentTagWrapper color={peer.isInterviewee ? "#D3635E" : "#61B153"}>
+      <FontAwesomeIcon icon={peer.isAudioOn ? faMicrophoneAlt : faMicrophoneAltSlash} />
+      {children}
+    </VideoContentTagWrapper>
+  );
+}
