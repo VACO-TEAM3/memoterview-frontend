cc19fc336ecc9fef96f6265f433b8af22fe90e56
Minjieun <chewchewchew3@gmail.com>
2021-04-28 16:18:29 +0900

    [FIX] right tab to left

---
+++ src/components/shared/StyledRightSideBar.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledRightSideBar = styled.nav`
+  height: 100%;
+  right: 0;
+  z-index: 1;
+  position: absolute;
+  margin-top: 10rem;
+`;
+
+export default StyledRightSideBar;
