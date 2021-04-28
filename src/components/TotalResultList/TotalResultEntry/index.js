7c79dfc9eb9941ac4a657ed4ead50d340fef7b50
hyerichung <hyerichungonearth@gmail.com>
2021-04-29 03:02:23 +0900

    [ADD] modal styles, detail and search page styles

--- src/components/TotalResultList/TotalResultEntry/index.js
+++ src/components/TotalResultList/TotalResultEntry/index.js
@@ -5,7 +5,7 @@ import TotalResultItem from "../TotalResultItem";
 
 const TotalResultEntryWrapper = styled.div`
   width: 100%;
-  height: 490px;
+  height: 480px;
   border: 1px solid ${({ theme }) => theme.LinkWater };
   border-radius: 2px;
   overflow-y: scroll;
