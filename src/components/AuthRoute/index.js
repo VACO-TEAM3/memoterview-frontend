6fdb4d39a7ee8aead4fd9544a7e47bd68de70f7f
ha-young <hayeong28@naver.com>
2021-04-29 19:38:10 +0900

    [ADD] logout

--- src/components/AuthRoute/index.js
+++ src/components/AuthRoute/index.js
@@ -4,12 +4,12 @@ import useToken from "../../hooks/useToken";
 
 export default function AuthRoute({ children, ...rest }) {
   const { token } = useToken(); // 이부분 로직 app container로 빼주기
-  
+
   return (
     <Route
       {...rest}
     >
-      {token ? children : <Redirect to={{ pathname: "/login", state: { from: rest.path } }} />}
+      {token ? children : <Redirect to={{ pathname: "/", state: { from: rest.path } }} />}
     </Route>
   );
 };
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,11 +5,20 @@ import interviewees, { intervieweeSaga } from "./interviewees";
 import projects, { projectsSaga } from "./projects";
 import user, { userSaga } from "./user";
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   user,
   projects,
   interviewees,
 });
+
+const rootReducer = (state, action) => {
+  let appState = state;
+  if (action.type === "LOGOUT") {
+    appState = undefined;
+  }
+
+  return appReducer(appState, action);
+};
 
 export function* rootSaga() {
   yield all([userSaga(), projectsSaga(), intervieweeSaga()]);
