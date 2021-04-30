b4bd37588ccca552a0427a8c4fd5c8e4de846864
ha-young <hayeong28@naver.com>
2021-04-30 19:37:43 +0900

    [FIX] body font import location

--- src/Layout/theme/GlobalStyles.js
+++ src/Layout/theme/GlobalStyles.js
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap");
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap');
     margin: 0;
     padding: 0;
     font-family: 'Gothic A1', 'Ubuntu', sans-serif;
